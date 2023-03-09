45d5b1fa9a6df42eae6dbd3b704fc0941e53fae8
trisha-thakur <trishathakur304@gmail.com>
2023-03-09 15:17:06 +0530

    intership section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Login from './assets/login'
+import Internship from './assets/Internship'
 
 import './App.css'
 
 function App() {
   
   return (
+    <div>
     <Login/>
+    <Internship/>
+    </div>
   )
 }
 
--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -1,4 +1,4 @@
-const Login = () => {
+ const Login = () => {
   return (
     
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
