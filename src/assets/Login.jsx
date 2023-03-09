158e1a6514aaa34fa59a42a335bf7388812a34c1
trisha-thakur <trishathakur304@gmail.com>
2023-03-09 13:00:56 +0530

    login page created

---
+++ src/assets/Login.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+
+
+ const Login = () => {
+        return ( 
+            <div className='max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl'>
+                <h2>Sign In</h2>
+            </div>
+         );
+    }
+     
+    
+export default Login ;
